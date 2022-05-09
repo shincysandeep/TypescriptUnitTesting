8ad93e77d08aeb95937507fd73a2dd0a873050da
Shincy Joseph <100630958+shincysandeep@users.noreply.github.com>
2022-05-09 11:56:59 -0500

    Add files via upload
    Initial commit

---
+++ calculator.ts
@@ -0,0 +1,8 @@
+
+export const add : Function = (a: number, b: number): number => a + b;
+export const subtract : Function = (a: number, b: number): number => a - b;
+export const divide : Function = (a: number, b: number): number => a / b;
+export const multiply : Function = (a: number, b: number): number => a * b;
+add(5,5);
+subtract(12,4);
+
